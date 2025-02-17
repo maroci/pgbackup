c230786b69dfd2287fb0e6fda55464e9b81e20fa
maroci <cirsir@posteo.net>
2025-02-17 15:38:01 +0100

    Create pgbackup package

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     description='A utility for backing up PostgreSQL databases',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/your_username/pgbackup',
+    url='https://github.com/maroci/pgbackup',
     packages=find_packages('src')
 )
